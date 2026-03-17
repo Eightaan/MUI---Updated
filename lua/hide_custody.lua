7954f9339c84b3db3dcaa5532e33efa28adfcaf2
Eightaan <57832730+Eightaan@users.noreply.github.com>
2026-03-17 15:48:38 +0100

    Delete lua/hide_custody.lua

--- lua/hide_custody.lua
+++
@@ -1,5 +0,0 @@
-Hooks:PostHook(PlayerManager, "change_stockholm_syndrome_count", "MUI_PlayerManager_change_stockholm_syndrome_count", function (self, ...)
-	if managers.hud and managers.hud._hud_player_custody and managers.hud._hud_player_custody.hide then
-		managers.hud._hud_player_custody:hide(false)
-	end
-end)
