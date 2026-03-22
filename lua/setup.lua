cbaebe1c6b6b630e7ba1138bf09a3e61223149af
Eightaan <57832730+Eightaan@users.noreply.github.com>
2026-03-22 22:24:58 +0100

    Add files via upload

--- lua/setup.lua
+++ lua/setup.lua
@@ -104,4 +104,169 @@ elseif RequiredScript == "lib/managers/trademanager" then
 			end
 		end
 	end)
+	
+--------------------- SETUP_CHALLENGE_NOTIFICATIONS ------------
+elseif RequiredScript == "lib/managers/hud/hudchallengenotification" then
+	if not (MUIMenu and MUIMenu:ClassEnabled("MUIPresent")) then return; end
+
+	function HudChallengeNotification:make_fine_text(text)
+		local x, y, w, h = text:text_rect();
+		text:set_size(w, h);
+		text:set_position(math.round(text:x()), math.round(text:y()));
+	end
+
+	function HudChallengeNotification:_animate_show(title_panel, text_panel)
+		local function fade(from, to, duration)
+			local t = duration;
+
+			while t > 0 do
+				coroutine.yield();
+				local dt = TimerManager:main():delta_time();
+				t = t - dt;
+
+				local a = math.lerp(from, to, 1 - (t / duration));
+				title_panel:set_alpha(a);
+				text_panel:set_alpha(a);
+			end
+
+			title_panel:set_alpha(to);
+			text_panel:set_alpha(to);
+		end
+
+		title_panel:set_alpha(0);
+		text_panel:set_alpha(0);
+
+		fade(0, 1, 0.2);
+		wait(3);
+		fade(1, 0, 0.2);
+
+		self:close();
+	end
+
+	function HudChallengeNotification:init(title, text, icon, rewards, queue)
+		if not MUIMenu._data.mui_challanges then return; end
+
+		self._ws = managers.gui_data:create_fullscreen_workspace();
+		self._muiFont = MUIMenu._data.mui_font_pref or 4;
+		self._font = ArmStatic.font_index(self._muiFont);
+		self._scale = (MUIMenu._data.mui_pst_size or 20) * 0.04;
+		HudChallengeNotification.super.init(self, self._ws:panel());
+		self._queue = queue or {};
+		self._hud = self._ws:panel();
+		self._hud:set_layer(1000);
+
+		local text_panel = self._hud:panel({});
+		local font_size = 20 * self._scale;
+
+		local noti_text = text_panel:text({
+			text = utf8.to_lower(text):gsub("^%l", string.upper) or "Blame overkill!",
+			font = self._font,
+			font_size = font_size,
+			vertical = "center",
+			w = 400 * self._scale,
+			wrap = true,
+			word_wrap = true,
+			layer = 2
+		});
+
+		self:make_fine_text(noti_text);
+		noti_text:set_h(noti_text:h() + 6 * self._scale);
+
+		local icon_texture, icon_texture_rect = tweak_data.hud_icons:get_icon_or(icon, nil);
+		local total_w = noti_text:w();
+
+		local row_h = noti_text:h();
+
+		if icon_texture then
+			local icon_h = 50 * self._scale;
+			row_h = math.max(noti_text:h(), icon_h);
+
+			local icon = text_panel:bitmap({
+				texture = icon_texture,
+				texture_rect = icon_texture_rect,
+				layer = 2,
+				w = 50 * self._scale,
+				h = 50 * self._scale
+			});
+
+			icon:set_left(0);
+			icon:set_center_y(row_h / 2);
+
+			noti_text:set_left(icon:right() + 5);
+			noti_text:set_center_y(row_h / 2);
+
+			total_w = icon:w() + 5 + noti_text:w();
+		else
+			noti_text:set_left(0);
+		end
+
+
+		text_panel:set_h(row_h);
+		text_panel:set_w(total_w);
+		text_panel:set_center_x(self._hud:w() / 2);
+
+		local box_height = noti_text:h();
+
+		for i, reward in ipairs(rewards or {}) do
+			local reward_panel = text_panel:panel({
+				h = 20,
+				y = noti_text:bottom() + (i - 1) * 22,
+				layer = 2
+			});
+
+			local icon_size = 20;
+			local icon_padding = 2;
+			local left_x = 0;
+
+			if reward.texture then
+				local reward_icon = reward_panel:bitmap({
+					x = left_x,
+					w = icon_size,
+					h = icon_size,
+					texture = reward.texture
+				});
+
+				reward_icon:set_center_y(icon_size / 2);
+			end
+
+			local text_x = left_x + icon_size + icon_padding;
+			
+			local reward_text_str = (reward.amount and (reward.amount .. "x ") or "") .. managers.localization:text(reward.name_id);
+
+			local reward_text = reward_panel:text({
+				text = reward_text_str,
+				font = self._font,
+				x = text_x,
+				layer = 2,
+				font_size = font_size
+			});
+
+			reward_text:set_center_y(icon_size / 2);
+			reward_panel:set_w(reward_text:right());
+			reward_panel:set_center_x(text_panel:w() / 2);
+
+			box_height = math.max(box_height, reward_panel:bottom() + 8);
+		end
+
+		local title_panel = self._hud:panel({});
+
+		local title_text = title_panel:text({
+			layer = 2,
+			text = utf8.to_lower(title):gsub("^%l", string.upper) or "Achievement unlocked!",
+			font = self._font,
+			font_size = font_size
+		});
+
+		self:make_fine_text(title_text);
+
+		title_panel:set_size(title_text:right(), title_text:bottom());
+
+		title_panel:set_bottom(self._hud:h() / 1.5);
+		title_panel:set_center_x(self._hud:w() / 2);
+
+		text_panel:set_top(title_panel:bottom());
+		text_panel:set_center_x(self._hud:w() / 2);
+
+		title_panel:animate(callback(self, self, "_animate_show"), text_panel);
+	end
 end
